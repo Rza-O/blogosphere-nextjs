375a19a5a19ee8a2dbaffb0d84987b19df66e4a4
appoclayptor <shahreza1414@gmail.com>
2025-01-23 21:22:21 +0600

    add Kinde authentication; create profile page and update Navbar for user session handling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,25 @@
 import { NavBar } from "./ui/tubelight-navbar";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
-const Navbar = () => {
+
+const Navbar = async () => {
+   const { getUser } = getKindeServerSession();
+   const user = await getUser();
+
+   console.log(user);
    const navItems = [
       { name: 'Home', url: '/' },
-      { name: 'Profile', url: '/' },
-      { name: 'Login', url: '/' },
+      { name: 'Profile', url: '/profile' },
+      { name: 'Login', url: '/api/auth/login' },
+   ];
+   const navSecure = [
+      { name: 'Home', url: '/' },
+      { name: 'Profile', url: '/profile' },
+      { name: 'Logout', url: '/api/auth/logout' },
    ];
    return (
       <div className='min-h-[70px]'>
-         <NavBar items={navItems}></NavBar>
+         <NavBar items={user? navSecure : navItems}></NavBar>
       </div>
    );
 }
