8429dcdbefe2c6085f41f75a58e3e8bc3996b719
appoclayptor <shahreza1414@gmail.com>
2025-01-23 22:24:00 +0600

    update app title to 'Blogosphere' and adjust layout margin on home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,11 @@
 import HomeCard from "@/components/HomeCard";
 import axios from "axios";
-import Image from "next/image";
 
 export default async function Home() {
   const { data: posts } = await axios('https://jsonplaceholder.typicode.com/posts');
   
   return (
-    <div className="min-h-screen mt-8 w-11/12 mx-auto 2xl:container">
+    <div className="min-h-screen mt-0 md:mt-8 w-11/12 mx-auto 2xl:container">
       <div className="text-center space-y-2">
         <h1 className="text-5xl">Welcome to Blogosphere!</h1>
         <p className="text-xl">Find all the latest blogs.</p>
